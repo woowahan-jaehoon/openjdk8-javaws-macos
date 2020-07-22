63e586b22c2bda489b6e2eacf7f3c862226c2f10
Jaehoon Jeong <jaehoon.jeong@woowahan.com>
2020-07-22 21:22:31 +0900

    change wget to curl

--- build.sh
+++ build.sh
@@ -11,9 +11,11 @@ if [[ ! -d ${SCRIPT_DIR}/tmp ]]; then
 fi
 cd ${SCRIPT_DIR}/tmp
 
+filename=icedtea-netx_1.8-0ubuntu8~18.04_amd64.deb
+
 echo
 echo "Downloading 'icedtea-netx_1.8' in Ubuntu 18.04 LTS ..."
-wget "http://security.ubuntu.com/ubuntu/pool/universe/i/icedtea-web/icedtea-netx_1.8-0ubuntu8~18.04_amd64.deb" 2> /dev/null
+curl -o ${filename} "http://security.ubuntu.com/ubuntu/pool/universe/i/icedtea-web/${filename}" 2> /dev/null
 
 if [[ $? -ne 0 ]]; then
     >&2 echo
@@ -23,7 +25,7 @@ if [[ $? -ne 0 ]]; then
 fi
 
 echo Unpacking...
-tar xfz icedtea-netx_1.8-0ubuntu8~18.04_amd64.deb
+tar xfz ${filename}
 tar xfz data.tar.xz
 cp ./usr/share/icedtea-web/netx.jar ${SCRIPT_DIR}/lib
 
